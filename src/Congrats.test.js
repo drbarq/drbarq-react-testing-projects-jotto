fa0cd07ae7e9ecdd90389ce41da4a1bf0e60cf89
Joe Tustin <J.Tustin@gmail.com>
2020-08-16 10:46:55 -0600

    adding new tests

--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -1,7 +1,13 @@
 import React from "react";
-import Enzye, { shallow } from "enzyme";
-import EnzymeAdapter from "enzyme-adaper-react-16";
+import Enzyme, { shallow } from "enzyme";
+import EnzymeAdapter from "enzyme-adapter-react-16";
 
 import Congrats from "./Congrats";
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+test("renders without error", () => {});
+
+test("renders no text when the `success` prop is false", () => {});
+
+test("renders non-empty congrats message when `sucess` prop is true", () => {});
