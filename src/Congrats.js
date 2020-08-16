faf7350e41e31514b883fe65cbdffd3fc6a8570a
Joe Tustin <J.Tustin@gmail.com>
2020-08-16 12:23:05 -0600

    building new congrats component

--- src/Congrats.js
+++ src/Congrats.js
@@ -2,9 +2,20 @@ import React from "react";
 /**
  * Functional react component for congratulatory message.
  * @function
+ * @param {object} props - React.props
  * @returns {JSX.Element} - Rendered Component (or null if 'success' prop is present)
  */
 
-export default () => {
-  return <div></div>;
+export default (props) => {
+  if (props.success) {
+    return (
+      <div data-test="component-congrats">
+        <span data-test="congrats-message">
+          Congragulations! You guessed the word!
+        </span>
+      </div>
+    );
+  } else {
+    return <div data-test="component-congrats" />;
+  }
 };
