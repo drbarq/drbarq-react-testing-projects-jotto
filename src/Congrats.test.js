9f464bc91c2ab8b1d3d1c3d3f1ed1eb8da57c24e
Joe Tustin <J.Tustin@gmail.com>
2020-08-16 12:55:18 -0600

    adding in new test setups

--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -1,10 +1,9 @@
 import React from "react";
-import Enzyme, { shallow, ShallowWrapper } from "enzyme";
-import EnzymeAdapter from "enzyme-adapter-react-16";
+import { shallow } from "enzyme";
+
 import Congrats from "./Congrats";
 import { findByTestAttr, checkProps } from "../test/testUtils";
 
-Enzyme.configure({ adapter: new EnzymeAdapter() });
 /**
  * Factory function to create ShallowWrapper for the Congrats component.
  * @function setup
@@ -18,19 +17,16 @@ const setup = (props = {}) => {
   const setupProps = { ...defaultProps, ...props };
   return shallow(<Congrats {...setupProps} />);
 };
-
 test("renders without error", () => {
   const wrapper = setup();
   const component = findByTestAttr(wrapper, "component-congrats");
   expect(component.length).toBe(1);
 });
-
 test("renders no text when the `success` prop is false", () => {
   const wrapper = setup({ success: false });
   const component = findByTestAttr(wrapper, "component-congrats");
   expect(component.text()).toBe("");
 });
-
 test("renders non-empty congrats message when `sucess` prop is true", () => {
   const wrapper = setup({ success: true });
   const message = findByTestAttr(wrapper, "congrats-message");
