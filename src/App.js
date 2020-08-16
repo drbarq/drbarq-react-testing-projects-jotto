6df2641f7d70f87444403a278cd82c054245712e
Joe Tustin <J.Tustin@gmail.com>
2020-08-16 17:19:26 -0600

    adding in new styling

--- src/App.js
+++ src/App.js
@@ -7,10 +7,12 @@ import Congrats from "./Congrats";
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Jotto</h1>
-        <Congrats success={false} />
-        <GuessedWords guessedWords={[]} />
+      <div className="container">
+        <h1 data-test="jotto-title">Jotto</h1>
+        <Congrats success={true} />
+        <GuessedWords
+          guessedWords={[{ guessedWord: "train", letterMatchCount: 3 }]}
+        />
       </div>
     );
   }
