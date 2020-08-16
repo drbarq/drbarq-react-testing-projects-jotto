c82956f0eb919de290b47c72dea38bab3e145a7e
Joe Tustin <J.Tustin@gmail.com>
2020-08-16 12:38:25 -0600

    adding in new tests and helper funcitons

--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -1,9 +1,8 @@
 import React from "react";
 import Enzyme, { shallow, ShallowWrapper } from "enzyme";
 import EnzymeAdapter from "enzyme-adapter-react-16";
-
 import Congrats from "./Congrats";
-import { findByTestAttr } from "../test/testUtils";
+import { findByTestAttr, checkProps } from "../test/testUtils";
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 /**
@@ -33,4 +32,8 @@ test("renders non-empty congrats message when `sucess` prop is true", () => {
   const wrapper = setup({ success: true });
   const message = findByTestAttr(wrapper, "congrats-message");
   expect(message.text().length).not.toBe(0);
+});
+test("does not throw warning with expected props", () => {
+  const expectedProps = { success: false };
+  checkProps(Congrats, expectedProps);
 });
--- test/testUtils.js
+++ test/testUtils.js
@@ -1,4 +1,6 @@
 import { ShallowWrapper } from "enzyme";
+import checkPropTypes from "check-prop-types";
+import { hasUncaughtExceptionCaptureCallback } from "process";
 
 /**
  * Return node(s) with the given data-test attribute.
@@ -9,4 +11,14 @@ import { ShallowWrapper } from "enzyme";
 
 export const findByTestAttr = (wrapper, val) => {
   return wrapper.find(`[data-test="${val}"]`);
+};
+
+export const checkProps = (component, conformingProps) => {
+  const propError = checkPropTypes(
+    component.propTypes,
+    conformingProps,
+    "prop",
+    component.name
+  );
+  expect(propError).toBeUndefinded;
 };
