2cca11914d1af1ef44bd4d28dfe801058d00fff4
Joe Tustin <J.Tustin@gmail.com>
2020-08-18 09:40:53 -0600

    adding in hook tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,19 +1,37 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { mount } from "enzyme";
+
 import { findByTestAttr } from "../test/testUtils";
 import App from "./App";
 
+import hookActions from "./actions/hookActions";
+
+const mockGetSecretWord = jest.fn();
+
 /**
  * Setup function for app component.
- * @returns { ShallowWrapper}
+ * @returns { ReactWrapper}
  */
 
 const setup = () => {
-  return shallow(<App />);
+  mockGetSecretWord.mockClear();
+  hookActions.getSecretWord = mockGetSecretWord;
+
+  // use mount, over shallow because jest does not run useEffect on shallow
+  // https://github.com/enzymejs/enzyme/issues/2086
+  return mount(<App />);
 };
 
 test("App renders without error", () => {
   const wrapper = setup();
   const component = findByTestAttr(wrapper, "component-app");
   expect(component.length).toBe(1);
+});
+
+describe("getSecretWord calls", () => {
+  test("getSecretWord gets called on App mount", () => {
+    setup();
+    // check to see if secretword was updated
+    expect(mockGetSecretWord).toHaveBeenCalled();
+  });
 });
