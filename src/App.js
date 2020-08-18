678648e20a458ee5212087d9e794f3e545e14efa
Joe Tustin <J.Tustin@gmail.com>
2020-08-18 10:17:26 -0600

    Creating reducer

--- src/App.js
+++ src/App.js
@@ -4,7 +4,27 @@ import "./App.css";
 import GuessedWords from "./GuessedWords";
 import Congrats from "./Congrats";
 
+/**
+ * reducer to update state, called automatically by dispatch
+ * @param {object} state - existing state
+ * @param {object} action - contains 'type' and 'payload' properties for the state updated
+ *                          for example : { type: "setSecrectWord", payload: "party"}
+ * @returns {object} - new state
+ */
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "setSecretWord":
+      return { ...state, secretWord: action.payload };
+
+    default:
+      throw new Error(`Invalid action type: ${action.type}`);
+  }
+}
+
 function App() {
+  const [state, dispatch] = React.useReducer(reducer, { secretWord: null });
+
   return (
     <div data-test="component-app" className="container">
       <h1 data-test="jotto-title">Jotto</h1>
